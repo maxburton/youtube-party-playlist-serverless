90fb63eec56e2caafb107fbe37a29f4564652658
Max <maxkburton@gmail.com>
2020-12-23 16:33:01 +0000

    Manually created dynamoDB table works

--- flask_yttp/__init__.py
+++ flask_yttp/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 
-
 # create and configure the app
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_mapping(
--- flask_yttp/video.py
+++ flask_yttp/video.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, flash, redirect, render_template, request, session, url_for
+    Blueprint, flash, redirect, render_template, request, session, url_for, jsonify
 )
 
 bp = Blueprint('video', __name__)
@@ -17,6 +17,7 @@ def index():
         if error is None:
             session["name"] = name
             if "makeroom" in request.form:
+                session["roomId"] = 1
                 return redirect(url_for('video.host'))
             elif "joinroom" in request.form:
                 return redirect(url_for('video.guest'))
@@ -25,9 +26,30 @@ def index():
 
     return render_template('video/index.html')
 
+import os
+import boto3
+
+ROOM_TABLENAME = "rooms"
+client = boto3.client('dynamodb')
+
 
 @bp.route('/host', methods=('GET', 'POST'))
 def host():
+    room_id = "1"
+    resp = client.get_item(
+        TableName=ROOM_TABLENAME,
+        Key={
+            'RoomId': {'N': room_id}
+        }
+    )
+    room_object = resp.get('Item')
+    if not room_object:
+        flash(f"Error: Room {room_id} does not exist")
+        return redirect(url_for('video.index'))
+
+    video_list = room_object["Videos"]["L"]
+    print(video_list)
+
     return render_template('video/host.html')
 
 
